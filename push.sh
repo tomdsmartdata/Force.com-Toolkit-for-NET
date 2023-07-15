a5d5905ea249177972cb4447cc814e469035cfc8
Tom Delaney <tom.delaney@enosix.com>
2023-07-14 21:28:42 -0400

    Update some branding

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
-dotnet nuget push artifacts/DeveloperForce.Force.$2.nupkg -k $1 -s https://api.nuget.org/v3/index.json
-
-dotnet nuget push artifacts/DeveloperForce.Chatter.$2.nupkg -k $1 -s https://api.nuget.org/v3/index.json
+dotnet nuget push artifacts/tdelaney.DeveloperForce.Force.$2.nupkg -k $1 -s https://api.nuget.org/v3/index.json
+dotnet nuget push artifacts/tdelaney.Salesforce.Common.$2.nupkg -k $1 -s https://api.nuget.org/v3/index.json
+dotnet nuget push artifacts/tdelaney.DeveloperForce.Chatter.$2.nupkg -k $1 -s https://api.nuget.org/v3/index.json
